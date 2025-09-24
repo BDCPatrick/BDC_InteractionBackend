e25acad811ae647091c84b64e5e9b5c3ddc1d896
BDC_Patrick <90676378+BDCPatrick@users.noreply.github.com>
2025-09-24 13:50:09 +0200

    Updated Instigator Definition - Draw Debug Cone

--- Source/BDC_InteractionBackend/Private/InteractionInstigator.cpp
+++ Source/BDC_InteractionBackend/Private/InteractionInstigator.cpp
@@ -14,16 +14,17 @@
 #include "Kismet/GameplayStatics.h"
 #include "Engine/World.h"
 #include "Engine/GameInstance.h"
+#include "DrawDebugHelpers.h"
 
 
 UInteractionInstigatorComponent::UInteractionInstigatorComponent()
 {
-	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
 }
 
 void UInteractionInstigatorComponent::BeginPlay()
 {
-    bAutoActivate = true;
 	Super::BeginPlay();
 
     if (UWorld* World = GetWorld())
@@ -36,4 +37,38 @@ void UInteractionInstigatorComponent::BeginPlay()
             }
         }
     }
+
+	SetComponentTickEnabled(bDrawDebug);
+}
+
+void UInteractionInstigatorComponent::SetDebugDrawing(bool bEnable)
+{
+	bDrawDebug = bEnable;
+	SetComponentTickEnabled(bDrawDebug);
+}
+
+void UInteractionInstigatorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (UWorld* World = GetWorld())
+	{
+		if (UGameInstance* GameInstance = World->GetGameInstance())
+		{
+			if (UBDC_InteractionSubsystem* InteractionSubsystem = GameInstance->GetSubsystem<UBDC_InteractionSubsystem>())
+			{
+				if(InteractionSubsystem->HasInstigatorLocationOverride())
+				{
+					const FVector Location = InteractionSubsystem->GetInstigatorLocation();
+					const float Rotation = InteractionSubsystem->GetInstigatorRotation();
+					const float Range = InteractionSubsystem->GetInteractionRange();
+					const float FallOffAngle = InteractionSubsystem->GetInteractionFallOff();
+
+					const FVector Direction = FRotationMatrix(FRotator(0.f, Rotation, 0.f)).GetUnitAxis(EAxis::X);
+
+					DrawDebugCone(World,Location,Direction,Range,FMath::DegreesToRadians(FallOffAngle),FMath::DegreesToRadians(FallOffAngle),12,FColor::Green,false,-1.f,0,1.f);
+				}
+			}
+		}
+	}
 }
